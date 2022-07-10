da250c8ae2c63775af7fa759a181f188972d8317
Thamognya Kodi <contact@thamognya.com>
2022-07-10 21:35:42 +0700

    done

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <h1 className="text-xl">Hello {'==>'}</h1>
+        </>
+    )
+}
+
+export default Home
